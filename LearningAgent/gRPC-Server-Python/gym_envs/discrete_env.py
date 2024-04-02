bb592c2c56a260d7300dce010fb058e2b11fe99c
Arad Firouzkouhi <aradfir@gmail.com>
2024-04-03 01:35:22 +0330

    edit discretization values

--- LearningAgent/gRPC-Server-Python/gym_envs/discrete_env.py
+++ LearningAgent/gRPC-Server-Python/gym_envs/discrete_env.py
@@ -8,7 +8,7 @@ import gymnasium as gym
 class DiscretePenaltyEnv(ContinuousPenaltyEnv):
     def __init__(self, verbose = False):
         self.ANGLE_DIVS = 12
-        self.POSSIBLE_KICK_VELS = [.1,.2,.3,.4,.5,.6,.7,.8] # relative to ball speed max
+        self.POSSIBLE_KICK_VELS = [.15,.25,.4,.6,1] # relative to ball speed max
         
         super().__init__(verbose)
         self.ANGLE_STEP = 360/self.ANGLE_DIVS
@@ -16,5 +16,9 @@ class DiscretePenaltyEnv(ContinuousPenaltyEnv):
 
     def gym_action_to_soccer_action(self, action, wm: WorldModel):
         angle = action // len(self.POSSIBLE_KICK_VELS) * self.ANGLE_STEP
-        kick_vel = self.POSSIBLE_KICK_VELS[action % len(self.POSSIBLE_KICK_VELS)] * self.server_param.ball_speed_max
-        return Body_KickOneStep(angle, kick_vel)
+        normalized_angle = (angle - 180)/180
+        kick_vel = self.POSSIBLE_KICK_VELS[action % len(self.POSSIBLE_KICK_VELS)]
+        action = [normalized_angle, kick_vel]
+        return super().gym_action_to_soccer_action(action, wm)
+    def calculate_reward(self, old_observation: State, action: np.ndarray, observation: State) -> float:
+        return super().calculate_reward(old_observation, action, observation)/10
