92a6c7d08d33c439d12743e861c72dc1c763dbc4
Arad Firouzkouhi <aradfir@gmail.com>
2024-04-03 01:36:04 +0330

    add new env for helios shoot

---
+++ LearningAgent/gRPC-Server-Python/gym_envs/discrete_with_helios_shoot_env.py
@@ -0,0 +1,24 @@
+
+from numpy import ndarray
+from gym_envs.discrete_env import DiscretePenaltyEnv
+import gymnasium as gym
+import service_pb2 as pb2
+
+from service_pb2 import WorldModel
+
+class DiscreteEnvWShoot(DiscretePenaltyEnv):
+    def __init__(self, verbose=False):
+        super().__init__(verbose)
+        self.action_space = gym.spaces.Discrete(self.ANGLE_DIVS * len(self.POSSIBLE_KICK_VELS) + 1)
+    
+    def gym_action_to_soccer_action(self, action, wm: WorldModel):
+        if action == 0:
+            return pb2.HeliosShoot()
+        return super().gym_action_to_soccer_action(action - 1, wm)
+    
+    def calculate_reward(self, old_observation: pb2.State, action: ndarray, observation: pb2.State) -> float:
+        shoot_reward = 0
+        if action == 0:
+            shoot_reward = -10
+        return super().calculate_reward(old_observation, action, observation) + shoot_reward
+        
